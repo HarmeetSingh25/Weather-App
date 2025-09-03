c2391339c6def9f95d0b72a1b738c5a90ce4e585
harmeet <159572239+codes-h7@users.noreply.github.com>
2025-09-03 16:13:10 +0530

    add feature if city is not find  then show error

--- script.js
+++ script.js
@@ -7,21 +7,34 @@ let input = document.getElementById("searchInput");
 let btn = document.getElementById("searchBtn");
 
 async function WeatherApiCaller(city) {
-  let reponse = await fetch(
-    ` https://api.weatherapi.com/v1/current.json?key=${ApiKey}&q=${city}`
-  );
+  try {
+    let reponse = await fetch(
+      ` https://api.weatherapi.com/v1/current.json?key=${ApiKey}&q=${city}`
+    );
+    console.log(reponse.ok);
+    if (!reponse.ok) {
+      document.querySelector(".error p").style.display = `block`;
+      document.querySelector(".WeatherStatusContainer").style.display = `none`;
+      throw new Error("City not found!");
+    }
+    let data = await reponse.json();
+    document.querySelector(".WeatherStatusContainer").style.display = `block`;
+    console.log(data);
+    Temp.textContent = `${data.current.temp_c}°C`;
+    City.textContent = `${data.location.name}`;
+    humidity.textContent = `${data.current.humidity}`;
+    wind.textContent = `${data.current.wind_kph} km/h`;
+    let p = (document.querySelector("#fail ").style.display = `none`);
+  } catch (error) {
+    document.querySelector(".WeatherStatusContainer").style.display = `none`;
 
-  let data = await reponse.json();
-  console.log(data);
-  Temp.textContent = `${data.current.temp_c}°C`;
-  City.textContent = `${data.location.name}`;
-  humidity.textContent = `${data.current.humidity}`;
-  wind.textContent = `${data.current.wind_kph} km/h`;
+    let p = document.querySelector("#fail ");
+    p.style.display = `block`;
+    p.textContent = "404 Error Found ";
+  }
 }
 
 btn.addEventListener("click", () => {
   WeatherApiCaller(input.value);
-  console.log(input);
   input.value = "";
-  document.querySelector(".WeatherStatusContainer").style.display = `block`;
 });
